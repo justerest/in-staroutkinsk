018f5b79f1ad46f14cdcde116b264f3033a17cc9
justerest <justerest@gmail.com>
2021-11-17 01:03:33 +0500

    chore: use scss for styled jsx

--- client/body-container.tsx
+++ client/body-container.tsx
@@ -1,15 +1,14 @@
 const BodyContainer: React.FC = (props) => (
   <>
     <div className="container">{props.children}</div>
+
     <style jsx>{`
       .container {
         max-width: 720px;
         margin: 0 auto;
         padding: 0 2rem;
-      }
 
-      @media screen and (max-width: 480px) {
-        .container {
+        @media screen and (max-width: 480px) {
           padding: 0 1rem;
         }
       }
